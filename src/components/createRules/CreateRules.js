f2d4e1ec9e9f9d1652487d03ffb206329ad6ea6c
Faiz Anwer <93755880+theanwerfaiz@users.noreply.github.com>
2024-05-02 20:31:07 +0530

    Update CreateRules.js

--- src/components/createRules/CreateRules.js
+++ src/components/createRules/CreateRules.js
@@ -1,7 +1,7 @@
 import React from "react";
 import BaseTemplate from "../BaseTemplate";
 const CreateRules = () => {
-  return <BaseTemplate>This is create Rules</BaseTemplate>;
+  return <BaseTemplate>This is Create Rules</BaseTemplate>;
 };
 
 export default CreateRules;
